c526839ecd2838b33a6cea0088bd9477bd4363cb
Maria Eduarda <madudarodrigues988@gmail.com>
2024-11-08 01:22:14 +0000

    finalização do aplicativo calculo do imc

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [peso,setPeso] = useState(0);
+  const [altura,setAltura] =useState(0);
+  const [imc,setImc] =useState(null);
+
+  const calcularImc = () =>{
+    var resultado =peso / (altura * altura)
+    setImc(resultado);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Aplicativo Caluculo IMC</Text>
-      <TextInput placeholder="Peso" keyboardType="numeric" />
+      <Text style={styles.titulo}>Aplicativo Caluculo IMC</Text>
+      <TextInput 
+      placeholder="Peso"
+      keyboardType="numeric"
+      value={peso}
+      onChangeText={(peso)=>setPeso(peso)}
+      />
 
-
+<TextInput 
+      placeholder="Altura"
+      keyboardType="numeric"
+      value={altura}
+      onChangeText={(altura)=>setAltura(altura)}
+      />
+      {imc&&(
+      <Text style={styles.titulo}>Imc: {imc}</Text>
+      )}
+      <Button title="Calcular" onPress={()=>{calcularImc()} }/>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +45,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tiulo: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
 });
